972f1bbd675d2565816fd6039f1fc8e755960d3f
jodorganistaca <jodorganistaca@unal.edu.co>
2019-09-24 18:06:31 -0500

    :construction: constraints

--- grails-app/domain/trip/planner/Trip.groovy
+++ grails-app/domain/trip/planner/Trip.groovy
@@ -6,4 +6,12 @@ class Trip {
     String finishDate
     String airline
     String place
+
+    static constraints = {
+        name maxSize: 255
+        startDate
+        finishDate
+        airline inList: ['Emirates', 'Avianca', 'Wingo', 'Aviatur', 'Latam', 'Despegar']
+        place nullable: true
+    }
 }
