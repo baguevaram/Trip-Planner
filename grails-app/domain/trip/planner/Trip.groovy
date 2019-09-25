71633c3f928d5b4cea75ce444000cd575b0c12fa
baguevaram <baguevaram@unal.edu.co>
2019-09-24 19:55:39 -0500

    :sparkles: rate a trip

--- grails-app/domain/trip/planner/Trip.groovy
+++ grails-app/domain/trip/planner/Trip.groovy
@@ -1,7 +1,19 @@
 package trip.planner
-
 class Trip {
+    int id
+    String name
+    String startDate
+    String finishDate
+    String airline
+    String place
+    double calification
 
     static constraints = {
+        name maxSize: 255
+        startDate
+        finishDate
+        airline inList: ['Emirates', 'Avianca', 'Wingo', 'Aviatur', 'Latam', 'Despegar']
+        place nullable: true
+        calification nullable: true
     }
 }
